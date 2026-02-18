19bfcda872078c113867bbec9346d92c1665746c
adwdawdawd <growlyakunsave@gmail.com>
2026-02-18 10:52:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -787,5 +788,6 @@ process.on('SIGINT', () => {
 });
 
 module.exports = app;
+
 
 
